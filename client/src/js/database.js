0976b7456b4a9863e6609ecf334ba4c5254033cd
Erin <50000677+sinclairems@users.noreply.github.com>
2024-05-06 18:42:28 -0500

    added logic to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,41 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    // Open the database connection
+    const jateDb = await openDB('jate', 1);
+    // Start a new transaction with read/write permissions
+    const tx = jateDb.transaction('jate', 'readwrite');
+    // Get a reference to the 'jate' object store
+    const store = tx.objectStore('jate');
+    // Use the .put() method to store the content
+    const request = store.put({ id: 1, value: content });
+    // Get confirmation of the successful request
+    const result = await request;
+    console.log('🚀 - data saved!', result);
+  } catch (error) {
+    console.error('putDB not implemented');
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    // Open the database connection
+    const jateDb = await openDB('jate', 1);
+    // Start a new transaction with readonly permissions
+    const tx = jateDb.transaction('jate', 'readonly');
+    // Get a reference to the 'jate' object store
+    const store = tx.objectStore('jate');
+    // Use the .get() method to retrieve the data by its id
+    const request = store.get(1);
+    // Get confirmation of the successful request
+    const result = await request;
+    // If a result is found, return it. If no result, return null
+    return result ? result.value : null;
+  } catch (error) {
+    console.error('getDb not implemented');
+  }  
+};
 
 initdb();
