488c4c86dfcf4413835523a2874de35418fbceeb
Erin <50000677+sinclairems@users.noreply.github.com>
2024-05-08 18:36:31 -0500

    chore: Add asset caching to service worker configuration

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,24 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+registerRoute(
+  ({ request }) =>
+    request.destination === "style" ||
+    request.destination === "script" ||
+    request.destination === "image",
+
+  new CacheFirst({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
 registerRoute();
